da543f2cd560a80febfeedfb51e700e48a3d8cfb
Omraan Timmerarends <omraan93@gmail.com>
2025-01-04 12:17:16 +0100

    reverted to stable state

--- src/arugas/index.ts
+++ src/arugas/index.ts
@@ -1,22 +1,14 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import haversine from "haversine-distance";
 import moment from "moment";
 require("dotenv").config();
-
-const getDistance = (
-	location1: { latitude: number; longitude: number },
-	location2: { latitude: number; longitude: number }
-) => {
-	return haversine(location1, location2);
-};
 
 export const getArugasData = async (date: string) => {
 	const Authorization = "Basic VXNyX0dQUy5BVzphc2YkR2ZlZzQyJEYxMjAx";
 	const environment = process.env["ENVIRONMENT"];
 	const organizationId = process.env[`${environment}_ARUGAS_ORG_ID`] as string;
 	try {
-		const response = await fetch(`https://portal.arugas.com/ARGGPS/ArugasService.svc/GetDispatch/${date}`, {
+		const response = await fetch(`https://portal.arugas.com/ARGGPS/ArugasService.svc/GetOrder/${date}`, {
 			headers: {
 				Authorization,
 			},
@@ -25,7 +17,7 @@ export const getArugasData = async (date: string) => {
 		if (!response.ok) {
 			throw new Error(`Server responded with a status of ${response.status}`);
 		}
-		const { DispatchList: data } = await response.json();
+		const { OrderList: data } = await response.json();
 		const trimmedData = data.map((item: any) => {
 			const trimmedItem: { [key: string]: any } = {};
 			Object.keys(item).forEach((key) => {
@@ -38,8 +30,6 @@ export const getArugasData = async (date: string) => {
 		await checkAndAddVehicles(trimmedData, organizationId);
 		await checkAndAddCustomers(trimmedData, organizationId);
 		await addOrders(trimmedData, organizationId, date);
-		await addDispatches(trimmedData, organizationId, date);
-		await addRoutes(organizationId, date);
 
 		return trimmedData;
 	} catch (error) {
@@ -105,8 +95,8 @@ const checkAndAddCustomers = async (trimmedData: any, organizationId: string) =>
 					city: "",
 					streetName: item.clientStreetName || "",
 					streetNumber: item.clientHouseNumber || "",
-					lat: item.latitude === "12.26512100" ? 0 : item.latitude,
-					lng: item.longitude === "-70.00457600" ? 0 : item.longitude,
+					lat: item.latitude,
+					lng: item.longitude,
 					phoneNumber: phoneNumbers[0] || "",
 					phoneNumber2: phoneNumbers[1] || "",
 					phoneNumber3: phoneNumbers[2] || "",
@@ -151,15 +141,30 @@ const addOrders = async (trimmedData: any, organizationId: string, dateString: s
 					// Voeg order toe
 					const newOrder = {
 						customerId,
+						vehicleId: item.vehicle,
+						expectedDeliveryDate: dateString,
 						orderNumber: item.orderNumber,
+						notes: item.notes || "",
+						status: "Open",
 						createdBy: "System",
 						createdAt: Number(new Date()),
+					};
+
+					const newOrderWithFirstEvent = {
+						...newOrder,
+						events: [
+							{
+								name: "Order Created",
+								description: "Order created and added to Order",
+								...newOrder,
+							},
+						],
 					};
 
 					const newOrderRef = ordersRef.push();
 					const newOrderId = newOrderRef.key;
 					if (newOrderId) {
-						updates[newOrderId] = newOrder;
+						updates[newOrderId] = newOrderWithFirstEvent;
 					}
 				} else {
 					console.error(
@@ -177,112 +182,6 @@ const addOrders = async (trimmedData: any, organizationId: string, dateString: s
 		console.log("All new orders have been pushed successfully.");
 	} catch (error) {
 		console.error("Error pushing new orders:", error);
-	}
-};
-
-const generateUniqueTrackAndTraceCode = async (): Promise<string> => {
-	let trackAndTraceCode: string = "";
-	let exists = true;
-	const db = admin.database();
-
-	while (exists) {
-		trackAndTraceCode = `VDPA${Math.random().toString(36).substr(2, 9).toUpperCase()}`;
-		const snapshot = await db.ref(`trackAndTraceIndex/${trackAndTraceCode}`).once("value");
-		exists = snapshot.exists();
-	}
-
-	return trackAndTraceCode;
-};
-
-const addDispatches = async (trimmedData: any, organizationId: string, dateString: string) => {
-	const db = admin.database();
-	const ordersSnapshot = await db.ref(`/organizations/${organizationId}/orders/date/${dateString}`).once("value");
-	const orders = ordersSnapshot.val() || {};
-
-	const vehiclesSnapshot = await db.ref(`/organizations/${organizationId}/vehicles`).once("value");
-	const vehicles = vehiclesSnapshot.val() || {};
-	const customerOrdersMap: { [customerId: string]: any[] } = {};
-
-	// Groepeer orders per klant
-	for (const orderId in orders) {
-		if (orders.hasOwnProperty(orderId)) {
-			const order = orders[orderId];
-			if (!customerOrdersMap[order.customerId]) {
-				customerOrdersMap[order.customerId] = [];
-			}
-			let input: { [key: string]: any } = { order, orderId };
-			const { vehicle, notes } = trimmedData.find((item: any) => item.orderNumber === order.orderNumber);
-			input.vehicleId = Object.keys(vehicles).find((key) => vehicles[key].licensePlate === vehicle) || "";
-			input.notes = notes || "";
-
-			customerOrdersMap[order.customerId].push(input);
-		}
-	}
-
-	const dispatchesRef = db.ref(`/organizations/${organizationId}/dispatches/date/${dateString}`);
-	const tntRef = db.ref(`/trackAndTraceIndex`);
-	const updatesDispatches: { [key: string]: any } = {};
-	const updatesTnt: { [key: string]: any } = {};
-
-	// Maak dispatches aan per klant
-	for (const customerId in customerOrdersMap) {
-		if (customerOrdersMap.hasOwnProperty(customerId)) {
-			const customerOrders = customerOrdersMap[customerId];
-			const trackAndTraceCode = await generateUniqueTrackAndTraceCode();
-
-			const newDispatch = {
-				organizationId,
-				trackAndTraceCode,
-				orderIds: customerOrders.map((order) => order.orderId),
-			};
-			// for (const dispatch of customerOrders) {
-			const newDispatchWithFirstEvent = {
-				...newDispatch,
-				customerId,
-				vehicleId: customerOrders[0].vehicleId,
-				notes: customerOrders[0].notes,
-				expectedDeliveryDate: dateString,
-				status: "Open",
-				createdAt: new Date().toISOString(),
-				createdBy: "System",
-				events: [
-					{
-						name: "Dispatch Created",
-						description: "Dispatch created and added to dispatch",
-						createdAt: new Date().toISOString(),
-						createdBy: "System",
-					},
-				],
-			};
-			const newDispatchRef = dispatchesRef.push();
-			const newDispatchId = newDispatchRef.key;
-			if (newDispatchId) {
-				updatesDispatches[newDispatchId] = newDispatchWithFirstEvent;
-			}
-			const newTnt = {
-				date: dateString,
-				dispatchId: newDispatchId,
-				organizationId,
-			};
-			const newTntRef = tntRef.push();
-			const newTntId = newTntRef.key;
-
-			if (newTntId) {
-				updatesTnt[newTntId] = newTnt;
-			}
-		}
-	}
-	try {
-		await dispatchesRef.update(updatesDispatches);
-		console.log("All new dispatches have been pushed successfully.");
-	} catch (error) {
-		console.error("Error pushing new dispatches:", error);
-	}
-	try {
-		await tntRef.update(updatesTnt);
-		console.log("All new T&T's have been pushed successfully.");
-	} catch (error) {
-		console.error("Error pushing new T&T's:", error);
 	}
 };
 
@@ -322,138 +221,3 @@ export const scheduledFetchArugasData = functions.pubsub
 		}
 		return null;
 	});
-
-// const MAPBOX_ACCESS_TOKEN = "sk.eyJ1Ijoib21yYWFuIiwiYSI6ImNtNTZuZXNjdjMwYTcya3A3dGIzbWZxcTYifQ.OKakG61fXWiiWVStGidjhw";
-
-interface LatLng {
-	latitude: number;
-	longitude: number;
-}
-
-const getOptimizedTrip = async (start: LatLng, destinations: LatLng[]) => {
-	const coordinates = [start, ...destinations].map((d) => `${d.longitude},${d.latitude}`).join(";");
-
-	const accessKey = process.env["ALL_FIREBASE_MAPBOX_KEY"];
-	const url = `https://api.mapbox.com/optimized-trips/v1/mapbox/driving/${coordinates}?source=first&destination=last&roundtrip=false&access_token=${accessKey}`;
-
-	const response: any = await fetch(url);
-	if (!response.ok) {
-		console.error(response);
-		throw new Error(`HTTP error! status: ${response.status}`);
-	}
-	const routeResponse: any = await response.json();
-	return routeResponse;
-};
-
-const addRoutes = async (organizationId: string, dateString: string) => {
-	const db = admin.database();
-
-	const customersRef = admin.database().ref(`/organizations/${organizationId}/customers`);
-	const customersSnapshot = await customersRef.once("value");
-	let customers = customersSnapshot.val() || {};
-
-	const dispatchRef = admin.database().ref(`/organizations/${organizationId}/dispatches/date/${dateString}`);
-	const dispatchesSnapshot = await dispatchRef.once("value");
-
-	const dispatches = dispatchesSnapshot.val() || {};
-	const vehiclesDispatchesMap: { [vehicleId: string]: any[] } = {};
-
-	// Groepeer dispatches per voertuig
-	for (const dispatchId in dispatches) {
-		if (dispatches.hasOwnProperty(dispatchId)) {
-			const dispatch = dispatches[dispatchId];
-			const vehicleId = dispatch.vehicleId;
-			if (!vehiclesDispatchesMap[vehicleId]) {
-				vehiclesDispatchesMap[vehicleId] = [];
-			}
-			vehiclesDispatchesMap[vehicleId].push({ ...dispatch, dispatchId });
-		}
-	}
-
-	let startLocation: LatLng = { latitude: 12.503286, longitude: -69.980893 };
-
-	// Maak routes aan per voertuig
-	for (const vehicleId in vehiclesDispatchesMap) {
-		if (vehiclesDispatchesMap.hasOwnProperty(vehicleId)) {
-			const vehicleDispatches = vehiclesDispatchesMap[vehicleId]
-				.map((item) => {
-					const { lat, lng } = customers[item.customerId];
-					const orderCoords = {
-						latitude: lat,
-						longitude: lng,
-					};
-					const distance = getDistance(startLocation, orderCoords);
-					return { ...item, distance, ...orderCoords };
-				})
-				.filter((row) => parseInt(row.latitude) !== 0)
-				.sort((a: any, b: any) => a.distance - b.distance);
-
-			// Verdeel dispatches in batches van maximaal 12 locaties
-			const batches = [];
-			for (let i = 0; i < vehicleDispatches.length; i += 11) {
-				const batch = vehicleDispatches.slice(i, i + 11);
-				batches.push(batch);
-			}
-
-			const routeDispatchIds: string[] = [];
-
-			for (const batch of batches) {
-				const routeResponse = await getOptimizedTrip(
-					startLocation,
-					batch.map((x) => {
-						return { latitude: x.latitude, longitude: x.longitude };
-					})
-				);
-
-				if (routeResponse.code !== "Ok") {
-					console.error("Error optimizing route:", routeResponse);
-					continue;
-				}
-				const { waypoints } = routeResponse;
-
-				const newDestinations = batch
-					.map((destination, indexDestination) => {
-						const relatedWaypoint = waypoints.find((waypoint: any, indexWaypoint: number) => {
-							// First waypoint is the start location, so we need to skip it.
-							return indexWaypoint - 1 === indexDestination;
-						});
-						return {
-							...destination,
-							waypoint_index: relatedWaypoint.waypoint_index,
-						};
-					})
-					.sort((a, b) => a.waypoint_index - b.waypoint_index);
-				routeDispatchIds.push(...newDestinations.map((x) => x.dispatchId));
-
-				startLocation = {
-					latitude: newDestinations[newDestinations.length - 1].latitude,
-					longitude: newDestinations[newDestinations.length - 1].longitude,
-				};
-			}
-
-			const route = {
-				vehicleId,
-				dispatchIds: routeDispatchIds,
-			};
-
-			const newRouteRef = await db.ref(`/organizations/${organizationId}/routes/date/${dateString}`).push(route);
-			const newRouteId = newRouteRef.key;
-
-			const updates: { [key: string]: any } = {};
-
-			for (const dispatchId in dispatches) {
-				if (dispatches.hasOwnProperty(dispatchId)) {
-					const dispatch = dispatches[dispatchId];
-					if (vehicleId === dispatch.vehicleId) {
-						const { vehicleId, ...rest } = dispatch;
-						updates[dispatchId] = {
-							...rest,
-							routeId: newRouteId,
-						};
-					}
-				}
-			}
-			dispatchRef.update(updates);
-		}
-	}
-};
